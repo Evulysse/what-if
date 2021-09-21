16015f82924ef4ae84ead6022df8c91ba79130e7
Esther Ulysse <estherulysse@Esthers-MBP.fios-router.home>
2021-09-21 12:19:01 -0400

    add node.js file

---
+++ what-can-you-do.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt-sync')();
+
+const age = Number(prompt('Enter a age'));
+
+if (age < 16){
+
+    console.log('You Cant drive');
+
+}
+
+else if (age  <= 17) {
+
+    console.log('You can drive but not vote');
+
+}
+else if (age <=18 && age <=24) {
+
+    console.log('You can vote but not rent a car');
+
+}
+else if (age >=25) {
+    console.log('You can do pretty much anything');
+}
+
